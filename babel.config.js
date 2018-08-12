8d74924081d580e80718440309408d748904d380
aschera <aschera@gmail.com>
2018-08-12 11:35:30 +0200

    deploy

---
+++ babel.config.js
@@ -0,0 +1,4 @@
+// transpiles javascript to support all features of our browsers.
+module.exports = {
+  presets: ["@vue/app"]
+};
---
+++ src/formdata.js
@@ -0,0 +1,23 @@
+{
+    name: "",
+    contact: {
+        main: {
+            mail: {
+                value: "",
+                placeholder: "email",
+            },
+            phone: {
+                value: "",
+                placeholder: "phone"
+            },
+            twitter: {
+                value: "",
+                placeholder: "twitter"
+            },
+        },
+    },
+    tags: "",
+    contactDetails: "",
+    contactNotes: "",
+    quality: "",
+}
